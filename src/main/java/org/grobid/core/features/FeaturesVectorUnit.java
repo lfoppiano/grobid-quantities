b05c70e404bc4f1d8b7d7f9d232eb9079e922353
Luca Foppiano <luca@foppiano.org>
2016-02-20 13:15:33 +0100

    Features for the Unit parser recognition

---
+++ src/main/java/org/grobid/core/features/FeaturesVectorUnit.java
@@ -0,0 +1,105 @@
+package org.grobid.core.features;
+
+import static org.apache.commons.lang3.StringUtils.isEmpty;
+import static org.apache.commons.lang3.StringUtils.isNotEmpty;
+
+/**
+ * Class representing the features for a unit recognition
+ * <p>
+ * Created by lfoppiano on 20.02.16.
+ */
+public class FeaturesVectorUnit {
+
+    public String value;            // lexical feature
+    public boolean isUpperCase = false;
+    public boolean isDigit = false;
+    public boolean isKnownUnitToken = false;
+    public String label = null;             // label if known
+
+    // one of NOPUNCT, OPENBRACKET, ENDBRACKET, DOT, COMMA, HYPHEN, QUOTE, PUNCT (default)
+    public String punctType = null;
+
+
+    public String printVector() {
+        if (isEmpty(value)) {
+            return null;
+        }
+        StringBuffer res = new StringBuffer();
+
+        res.append(value);
+
+        if (isUpperCase) {
+            res.append(" ").append(1);
+        } else {
+            res.append(" ").append(0);
+        }
+
+        if (isDigit) {
+            res.append(" ").append(1);
+        } else {
+            res.append(" ").append(0);
+        }
+
+        if (isKnownUnitToken) {
+            res.append(" ").append(1);
+        } else {
+            res.append(" ").append(0);
+        }
+
+        res.append(" ").append(punctType);
+
+        if (isNotEmpty(label)) {
+            res.append(" ").append(label);
+        }
+
+        return res.toString();
+    }
+
+    /**
+     * Add the features for the chemical entity extraction model.
+     */
+    static public FeaturesVectorUnit addFeaturesUnit(String character,
+                                                     String label,
+                                                     boolean isUnitToken) {
+        FeatureFactory featureFactory = FeatureFactory.getInstance();
+
+        FeaturesVectorUnit featuresVector = new FeaturesVectorUnit();
+
+        featuresVector.value = character;
+        featuresVector.label = label;
+
+        if (featureFactory.test_all_capital(character)) {
+            featuresVector.isUpperCase = true;
+        } else {
+            featuresVector.isUpperCase = false;
+        }
+
+        if ((character.equals("(")) || (character.equals("["))) {
+            featuresVector.punctType = "OPENBRACKET";
+        } else if ((character.equals(")")) || (character.equals("]"))) {
+            featuresVector.punctType = "ENDBRACKET";
+        } else if (character.equals(".")) {
+            featuresVector.punctType = "DOT";
+        } else if (character.equals(",")) {
+            featuresVector.punctType = "COMMA";
+        } else if (character.equals("-")) {
+            featuresVector.punctType = "HYPHEN";
+        } else if (character.equals("\"") || character.equals("\'") || character.equals("`")) {
+            featuresVector.punctType = "QUOTE";
+        } else {
+            featuresVector.punctType = "NOPUNCT";
+        }
+
+        if (featureFactory.test_number(character)) {
+            featuresVector.isDigit = true;
+        } else {
+            featuresVector.isDigit = false;
+        }
+
+        featuresVector.isKnownUnitToken = isUnitToken;
+
+        return featuresVector;
+    }
+
+
+}
---
+++ src/test/java/org/grobid/core/features/FeaturesVectorUnitTest.java
@@ -0,0 +1,46 @@
+package org.grobid.core.features;
+
+import org.grobid.core.main.LibraryLoader;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * Created by lfoppiano on 20.02.16.
+ */
+public class FeaturesVectorUnitTest {
+
+    @Before
+    public void setUp() throws Exception {
+        LibraryLoader.load();
+    }
+
+    @Test
+    public void testPrintVector_sample1() throws Exception {
+        FeaturesVectorUnit output = FeaturesVectorUnit.addFeaturesUnit("c", "LABEL", false);
+        String outputString = output.printVector();
+
+        assertThat(outputString, is("c 0 0 0 NOPUNCT LABEL"));
+    }
+
+    @Test
+    public void testPrintVector_sample2() throws Exception {
+        FeaturesVectorUnit output = FeaturesVectorUnit.addFeaturesUnit("2", "LABEL", false);
+        String outputString = output.printVector();
+
+        assertThat(outputString, is("2 1 1 0 NOPUNCT LABEL"));
+    }
+
+    @Test
+    public void testAddFeaturesUnit() throws Exception {
+        FeaturesVectorUnit output = FeaturesVectorUnit.addFeaturesUnit("c", null, false);
+
+        assertNotNull(output.isDigit);
+        assertNotNull(output.isKnownUnitToken);
+        assertNotNull(output.isUpperCase);
+        assertNotNull(output.punctType);
+        assertNull(output.label);
+    }
+}
