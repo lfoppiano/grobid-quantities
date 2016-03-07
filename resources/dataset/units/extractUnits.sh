0bab7e03e77d914d56e94092ef411dc1583d72f6
Luca Foppiano <luca@foppiano.org>
2016-03-07 10:43:38 +0100

    SMall corrections to the quantities training data, adding new training examples for units

--- resources/dataset/units/extractUnits.sh
+++ resources/dataset/units/extractUnits.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-grep -oh -E '<measure type=\"[A-Z]+\" unit=\"[^<]+\">([^<]+)</measure>' *.tei.xml | sed 's/<\/measure>//g' | sed -r 's/<measure type=\"[A-Z]+\" unit=\"[^<]+\">//g'
+INPUT=$1
+
+if [ -z "${INPUT}" ]; then 
+echo "Missing filename. Usage extractUnits.sh *.xml"; 
+exit 0
+fi
+
+grep -oh -E '<measure type=\"[A-Z]+\" unit=\"[^<]+\">([^<]+)</measure>' ${INPUT} | sed 's/<\/measure>//g' | sed -r 's/<measure type=\"[A-Z]+\" unit=\"[^<]+\">//g'
