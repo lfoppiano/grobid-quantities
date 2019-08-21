3597cf684201231c3fbe458825bb5904ae562bca
Luca Foppiano <luca@foppiano.org>
2019-08-21 11:04:54 +0900

    minor improvements on the text chunk sax handler

--- src/main/java/org/grobid/core/sax/TextChunkSaxHandler.java
+++ src/main/java/org/grobid/core/sax/TextChunkSaxHandler.java
@@ -9,20 +9,20 @@ import java.util.List;
 
 /**
  * Relatively stupid SAX parser which identify structural chunk of textual content like paragraph, titles, ...
- * The parser should "work" (at the expected level of stupidity) with many usual formats like TEI and ST.36. 
- * 
+ * The parser should "work" (at the expected level of stupidity) with many usual formats like TEI and ST.36.
+ *
  * @author Patrice Lopez
  */
 public class TextChunkSaxHandler extends DefaultHandler {
 
     StringBuffer accumulator = new StringBuffer(); // Accumulate parsed text
 
-    private List<String> filters = null; // the name of an element for filtering out the
-                                        // corresponding text, e.g. figure
+    private List<String> filteredTags = new ArrayList<>(); // the name of an element for filtering out the
+    // corresponding text, e.g. figure
 
     private boolean accumule = true;
 
-    public List<String> chunks = null;
+    public List<String> chunks = new ArrayList<>();
 
     public TextChunkSaxHandler() {
     }
@@ -33,15 +33,13 @@ public class TextChunkSaxHandler extends DefaultHandler {
         }
     }
 
-    public void setFilter(List<String> filt) {
-        filters = filt;
+    public void setFilteredTags(List<String> filt) {
+        filteredTags = filt;
         accumule = false;
     }
 
-    public void addFilter(String filt) {
-        if (filters == null)
-            filters = new ArrayList<String>();
-        filters.add(filt);
+    public void addFilteredTag(String filt) {
+        filteredTags.add(filt);
         accumule = false;
     }
 
@@ -53,8 +51,8 @@ public class TextChunkSaxHandler extends DefaultHandler {
         String text = accumulator.toString().trim();
         text = text.replace("\n", " ");
         text = text.replace("\t", " ");
-        text = text.replace(" ", " "); 
-        // the last one is a special "large" space missed by the regex "\\p{Space}+" bellow
+        text = text.replace(" ", " ");
+        // the last one is a special "large" space missed by the regex "\\p{Space}+" below
         text = text.replaceAll("\\p{Space}+", " ");
         return text;
     }
@@ -63,24 +61,22 @@ public class TextChunkSaxHandler extends DefaultHandler {
             throws SAXException {
         if (accumule) {
             if (qName.equals("p") || qName.equals("paragraph")) {
-                if (chunks == null) 
-                    chunks = new ArrayList<String>();
                 chunks.add(getText());
                 accumulator.setLength(0);
             }
         }
-        if ((filters != null) && filters.equals(qName)) {
+        if (filteredTags.contains(qName)) {
             accumule = true;
         }
     }
 
     public void startElement(String namespaceURI, String localName,
-            String qName, Attributes atts) throws SAXException {
-        if ((filters != null) && filters.equals(qName)) {
+                             String qName, Attributes atts) throws SAXException {
+        if (filteredTags.contains(qName)) {
             accumule = false;
-        }
-        else 
+        } else {
             accumule = true;
+        }
 
         if (qName.equals("p") || qName.equals("paragraph")) {
             accumulator.setLength(0);
