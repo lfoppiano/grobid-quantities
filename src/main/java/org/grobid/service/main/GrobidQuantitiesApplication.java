d3e858e38bf6237a86a30c7b9d3623d9e7c80e9f
Luca Foppiano <luca@foppiano.org>
2019-12-25 14:56:41 +0900

    Cors configuration #102

--- src/main/java/org/grobid/service/main/GrobidQuantitiesApplication.java
+++ src/main/java/org/grobid/service/main/GrobidQuantitiesApplication.java
@@ -8,6 +8,7 @@ import io.dropwizard.assets.AssetsBundle;
 import io.dropwizard.forms.MultiPartBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.grobid.service.QuantitiesServiceModule;
 import org.grobid.service.command.UnitBatchProcessingCommand;
 import org.grobid.service.command.TrainingGenerationCommand;
@@ -15,6 +16,9 @@ import org.grobid.service.configuration.GrobidQuantitiesConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
 import java.util.List;
 
 public class GrobidQuantitiesApplication extends Application<GrobidQuantitiesConfiguration> {
@@ -46,7 +50,24 @@ public class GrobidQuantitiesApplication extends Application<GrobidQuantitiesCon
 
     @Override
     public void run(GrobidQuantitiesConfiguration configuration, Environment environment) {
+        LOGGER.info("Service config={}", configuration);
         environment.jersey().setUrlPattern(RESOURCES + "/*");
+
+        String allowedOrigins = configuration.getCorsAllowedOrigins();
+        String allowedMethods = configuration.getCorsAllowedMethods();
+        String allowedHeaders = configuration.getCorsAllowedHeaders();
+
+        // Enable CORS headers
+        final FilterRegistration.Dynamic cors =
+            environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+        // Configure CORS parameters
+        cors.setInitParameter("allowedOrigins", allowedOrigins);
+        cors.setInitParameter("allowedHeaders", allowedMethods);
+        cors.setInitParameter("allowedMethods", allowedHeaders);
+
+        // Add URL mapping
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, RESOURCES + "/*");
     }
 
     public static void main(String[] args) throws Exception {
