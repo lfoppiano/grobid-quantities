09b087aa2f67d99549f50ddb87b9feae1eae729d
Luca Foppiano <Foppiano.Luca@nims.go.jp>
2023-01-18 14:32:50 +0900

    add version and revision in the API

--- src/main/java/org/grobid/service/configuration/GrobidQuantitiesConfiguration.java
+++ src/main/java/org/grobid/service/configuration/GrobidQuantitiesConfiguration.java
@@ -2,12 +2,21 @@ package org.grobid.service.configuration;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
+import org.apache.commons.io.IOUtils;
 import org.grobid.core.utilities.GrobidConfig;
+import org.grobid.core.utilities.GrobidProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
 public class GrobidQuantitiesConfiguration extends Configuration {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(GrobidQuantitiesConfiguration.class);
 
     private String grobidHome;
 
@@ -17,6 +26,15 @@ public class GrobidQuantitiesConfiguration extends Configuration {
     private String corsAllowedMethods = "OPTIONS,GET,PUT,POST,DELETE,HEAD";
     @JsonProperty
     private String corsAllowedHeaders = "X-Requested-With,Content-Type,Accept,Origin";
+
+
+    // Version
+    private static String VERSION = null;
+    private static String REVISION = null;
+    private static final String UNKNOWN_VERSION_STR = "unknown";
+    private static final String GROBID_VERSION_FILE = "/version.txt";
+    private static final String GROBID_REVISION_FILE = "/revision.txt";
+
 
     private List<GrobidConfig.ModelParameters> models = new ArrayList<>();
     private int maxParallelRequests;
@@ -68,6 +86,40 @@ public class GrobidQuantitiesConfiguration extends Configuration {
             this.maxParallelRequests = Runtime.getRuntime().availableProcessors();
         }
         return this.maxParallelRequests;
+    }
+
+    public static String getVersion() {
+        if (VERSION != null) {
+            return VERSION;
+        }
+        synchronized (GrobidProperties.class) {
+            if (VERSION == null) {
+                VERSION = readFromFile(GROBID_VERSION_FILE);
+            }
+        }
+        return VERSION;
+    }
+
+    public static String getRevision() {
+        if (REVISION != null) {
+            return REVISION;
+        }
+        synchronized (GrobidProperties.class) {
+            if (REVISION == null) {
+                REVISION = readFromFile(GROBID_REVISION_FILE);
+            }
+        }
+        return REVISION;
+    }
+
+    private static String readFromFile(String filePath) {
+        String grobidVersion = UNKNOWN_VERSION_STR;
+        try (InputStream is = GrobidProperties.class.getResourceAsStream(filePath)) {
+            grobidVersion = IOUtils.toString(is, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            LOGGER.error("Cannot read the version from resources", e);
+        }
+        return grobidVersion;
     }
 
     public String getCleanlpModelPath() {
